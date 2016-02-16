043ecbdc248b43633a4ef9ea202054b59cecf080
abkendal <kendal.adam@mail.com>
2016-02-15 21:34:48 -0500

    Update site

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$(function(){
 
+
+
+	// init Masonry
+	var $grid = $('.grid').masonry({
+	  itemSelector: '.grid-item',
+	  columnWidth: 300,
+	  fitWidth: true
+	});
+
+	// layout Masonry after each image loads
+	$grid.imagesLoaded().progress( function() {
+	  $grid.masonry('layout');
+	});
+
+	$("img.lazy").lazyload();
+});
